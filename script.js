113af600da68e86f805c8590f590498edb97bfe3
BalakumaranDhandapani <113333673+BalakumaranDhandapani@users.noreply.github.com>
2022-10-19 23:59:00 +0530

    Call Back Hell Task

---
+++ script.js
@@ -0,0 +1,36 @@
+const ele = document.querySelector(".hell");
+
+
+setTimeout(() => {
+    ele.innerHTML="10";
+    setTimeout(() => {
+        ele.innerHTML="9";
+        setTimeout(() => {
+            ele.innerHTML="8";
+            setTimeout(() => {
+                ele.innerHTML="7";
+                setTimeout(() => {
+                    ele.innerHTML="6";
+                    setTimeout(() => {
+                        ele.innerHTML="5";
+                        setTimeout(() => {
+                            ele.innerHTML="4";
+                            setTimeout(() => {
+                                ele.innerHTML="3";
+                                setTimeout(() => {
+                                    ele.innerHTML="2";
+                                    setTimeout(() => {
+                                        ele.innerHTML="1";
+                                        setTimeout(() => {
+                                            ele.innerHTML="Happy Independence Day!!!";
+                                        }, 1000);
+                                    }, 1000);
+                                }, 1000);
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}, 1000);
